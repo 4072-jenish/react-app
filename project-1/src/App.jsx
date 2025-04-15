d784f0b7195bcce9674270a9bf91fd4fd87f3729
Jenish hariyani <hariyanijenish@gmail.com>
2025-04-15 11:31:46 +0530

    project - update 15/04/2025

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import  Profile from './components/user-profile';
+import image1 from  './assets/user-1.png';
+import image2 from  './assets/user-2.webp';
+import image3 from  './assets/user-3.jpg';
+import image4 from  './assets/user-4.webp';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div>
+     < Profile  image={image1}  name="John Doe" age={30} email="johndoe@example.com" phone="123-456-7890" />
+     < Profile  image={image2}  name="jinwoo sung" age={25} email="jinwoo@example.com" phone="986532741850" />
+     < Profile  image={image3}  name="peter parker" age={22} email="peter@example.com" phone="8756412365" />
+     < Profile  image={image4}  name="robert daoury jr" age={45} email="daoury@example.com" phone="8466135455" />
+    </div>
   )
 }
 
---
+++ project-1/src/components/user-profile.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import  "./user.css";
+import '../assets/user-1.png'
+
+class Profile extends React.Component {
+    
+   constructor(props) {
+      super(props);
+}
+    render() {
+        return(
+            <div>
+
+<div class="container">
+      <section>
+        <div class="items-inner">
+          <div class="items">
+            <div class="item">
+              <div class="photo">
+                    <img src={this.props.image}/>
+                <div class="wish" div></div>
+              </div>
+              <div class="desc">
+                <small>User name : {this.props.name}</small>
+                <span> Uaer Email : {this.props.email}</span>
+                <h5> User phone : {this.props.phone}</h5>
+                <em>User age : {this.props.age}</em>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+            </div>
+        )
+    }
+}
+
+export default Profile;
