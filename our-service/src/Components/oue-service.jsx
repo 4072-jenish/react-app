2c628a7273b5f0cdd7e14ee5859180515db4131a
Jenish hariyani <hariyanijenish@gmail.com>
2025-05-05 12:00:21 +0530

    update 05/2025

--- our-service/src/Components/oue-service.jsx
+++ our-service/src/Components/oue-service.jsx
@@ -7,6 +7,7 @@ import servie1 from "../assets/image/service-card-img-1.jpg";
 import servie2 from "../assets/image/service-card-img-2.jpg";
 import servie3 from "../assets/image/service-card-img-3.jpg";
 import SaleComp from './sale-comp/sale-comp';
+import TestimonialSlider from "./slider/sliderComp";
 
 
 function OurService() {
@@ -19,6 +20,8 @@ function OurService() {
     <ServicesCard  title="Furniture Design" description="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. sed ut perspici" image={servie2}/>
     <ServicesCard title="Handmade Craft" description="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. sed ut perspici" image={servie3}/>
     <SaleComp/>
+    <TestimonialSlider />
+    {/* ReactDOM.render(, document.getElementById("root")); */}
     </>
   )
 }
