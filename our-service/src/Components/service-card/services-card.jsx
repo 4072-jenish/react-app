ca50df34b9b33658b73befd15f3cc181aa2fce3e
Jenish hariyani <hariyanijenish@gmail.com>
2025-05-12 10:48:06 +0530

    card align update 12/05/2025

--- our-service/src/Components/service-card/services-card.jsx
+++ our-service/src/Components/service-card/services-card.jsx
@@ -8,13 +8,13 @@ const ServicesCard = (props) => {
   return(
     <>
         <div className="container">
-           <div className="card-servce d-flex justify-content-between align-items-center flex-wrap" style={{marginTop: "100px"}} >
+           <div className="card-servce d-flex justify-content-around align-items-center flex-wrap" style={{marginTop: "100px"}} >
               <div className="card-image col-md-6 col-sm-8 col-12 shine-hover">
                 <img src={props.image} alt="service" className="image-fluid" />
               </div>
             <div className="card-content col-md-6 col-sm-4 col-12 ps-sm-5 pt-2  text-center text-sm-start">
               <h1 className="mb-4">{props.title}</h1>
-              <p className="pb-3">{props.description}</p>
+              <p className="pb-5">{props.description}</p>
               <button className="btn-hover fw-semibold">View Catalog  <CgArrowTopRight/></button>
             </div>
            </div>
