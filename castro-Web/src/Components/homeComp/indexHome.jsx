0f4562a9883882eb9ddd9a5eca119a398e82406b
4072-jenish <150036720+4072-jenish@users.noreply.github.com>
2025-11-13 17:38:21 +0530

    update

--- castro-Web/src/Components/homeComp/indexHome.jsx
+++ castro-Web/src/Components/homeComp/indexHome.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './Components/home'
+import Home from './Components/Home'
 <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Poppins&display=swap" rel="stylesheet"></link>
 
 
